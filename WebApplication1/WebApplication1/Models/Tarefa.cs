90dbcea7b1528073a8b7d3b60f91db1e4ac95199
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2021-12-16 14:39:14 -0300

    Criando primeiro classe

--- WebApplication1/WebApplication1/Models/Tarefa.cs
+++ WebApplication1/WebApplication1/Models/Tarefa.cs
@@ -2,5 +2,17 @@
 {
     public class Tarefa
     {
+        public string Id { get; private set; }
+
+        public string Nome { get; private set; }
+
+        public string Detalhes { get; private set; }
+
+        public bool Concluido { get; private set; }
+
+
+        public string DateTime DataCadastro { get; private set; }
+        
+        public string DateTime? DataConclusao { get; private set; }
     }
 }
